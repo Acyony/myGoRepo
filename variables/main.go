14e5fb5019d0fadd84bc855d1c786b8c3d6a2d67
Alcione <alcionefribeiro@gmail.com>
2021-12-18 22:59:55 +0100

    add aplayTheGame function

--- variables/main.go
+++ variables/main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
-const prompt = "and press ENTER when ready."
+const prompt = "and don't type your number in, just press ENTER when ready."
 
 func main() {
-	var firstNumber = 2
-	var secondNumber = 5
-	var subtraction = 7
-	//var answer int
+	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	var firstNumber = rand.Intn(8) + 2
+	var secondNumber = rand.Intn(8) + 2
+	var subtraction = rand.Intn(8) + 2
+	var answer = firstNumber*secondNumber - subtraction
+	playTheGame(firstNumber, secondNumber, subtraction, answer)
+}
+
+func playTheGame(firstNumber, secondNumber, subtraction, answer int) {
+
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Guess the Number Game")
 	fmt.Println("---------=^.^=-----------")
@@ -33,4 +43,6 @@ func main() {
 	fmt.Println("Now subtract", subtraction, prompt)
 	reader.ReadString('\n')
 
+	//Give them the answer
+	fmt.Println("The answer is", answer)
 }
