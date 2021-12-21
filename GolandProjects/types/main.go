701801be6e95268983647da51501c617c7defda8
Alcione <alcionefribeiro@gmail.com>
2021-12-21 22:45:44 +0100

    learninf aggregate types

--- GolandProjects/types/main.go
+++ GolandProjects/types/main.go
@@ -3,15 +3,27 @@ package main
 import "fmt"
 
 // aggregate types(array, struct)
+// struct is considered an aggregate type, because it can hold many pieces of information
 // reference types(pointers, slices, maps, functions, channels)
 // interface type
 
-func main() {
-	// declaring array in goLang
-	var myStrings [3]string
-	myStrings[0] = "cat"
-	myStrings[1] = "dog"
-	myStrings[2] = "fish"
+type Car struct {
+	NumberOfTires int
+	Luxury        bool
+	BucketSeats   bool
+	Make          string
+	Model         string
+	Year          int
+}
 
-	fmt.Println("The first element in thr array is:", myStrings[0])
+func main() {
+	myCar := Car{
+		NumberOfTires: 4,
+		Luxury:        true,
+		BucketSeats:   true,
+		Make:          "Volvo",
+		Model:         "XX2021",
+		Year:          2021,
+	}
+	fmt.Printf("My car is a %d %s %s", myCar.Year, myCar.Make, myCar.Model)
 }
