03cac20daf8ec3ac5aa7a08cac44ae2be3d5edfc
Alcione <alcionefribeiro@gmail.com>
2021-12-21 22:34:47 +0100

    declaring arrays

--- GolandProjects/types/main.go
+++ GolandProjects/types/main.go
@@ -1,31 +1,17 @@
 package main
 
-import "log"
-
-// basic types (numbers, strings, booleans)
-var myInt int
-var myUint uint
-var myFloat float32
-var myFloat64 float64
+import "fmt"
 
 // aggregate types(array, struct)
 // reference types(pointers, slices, maps, functions, channels)
 // interface type
 
 func main() {
-	myInt = 10
-	myUint = 20
-	myFloat = 10.5
-	myFloat64 = 100.1
+	// declaring array in goLang
+	var myStrings [3]string
+	myStrings[0] = "cat"
+	myStrings[1] = "dog"
+	myStrings[2] = "fish"
 
-	log.Println(myInt, myUint, myFloat, myFloat64)
-
-	myString := "Alcione"
-	log.Println(myString)
-	// strings in go are immutables
-	myString = "new name"
-
-	var myBool = true
-	myBool = false
-	log.Println(myBool)
+	fmt.Println("The first element in thr array is:", myStrings[0])
 }
