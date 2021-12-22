b24511029856a48b4f9ef3c105b87eef7b764217
Alcione <alcionefribeiro@gmail.com>
2021-12-22 20:15:52 +0100

    learning go functions

--- GolandProjects/types/main.go
+++ GolandProjects/types/main.go
@@ -5,30 +5,47 @@ import "fmt"
 // reference types(pointers, slices, maps, functions, channels)
 // interface type
 
-func main() {
-	intMap := make(map[string]int)
-	intMap["one"] = 1
-	intMap["two"] = 2
-	intMap["three"] = 3
-	intMap["four"] = 4
-	intMap["five"] = 5
+//assigning functions to types =^_^=
 
-	for key, value := range intMap {
-		fmt.Println(key, value)
-	}
-
-	//delete(intMap, "four")
-
-	// finding if an element exist in a map
-	el, ok := intMap["four"]
-	if ok {
-		fmt.Println(el, "is in map")
-	} else {
-		fmt.Println(el, "is not in map")
-	}
-
-	// change the value of an element in a map
-	intMap["two"] = 4
+type Animal struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
 }
 
-// not possible to sort a map
+func (a *Animal) Says() {
+	fmt.Printf("A %s says %s", a.Name, a.Sound)
+	fmt.Println()
+}
+
+func (a *Animal) HowManyLegs() {
+	fmt.Printf("A %s has %d legs", a.Name, a.NumberOfLegs)
+	fmt.Println()
+}
+
+func main() {
+	var dog Animal
+	dog.Name = "dog"
+	dog.Sound = "woof"
+	dog.NumberOfLegs = 4
+	dog.Says()
+
+	cat := Animal{
+		Name:         "cat",
+		Sound:        "meow",
+		NumberOfLegs: 4,
+	}
+	cat.Says()
+	cat.HowManyLegs()
+}
+
+/*func sumMany(nums ...int) int {
+	total := 0
+
+	for _, x := range nums {
+		total = total + x
+	}
+
+	return total
+}
+*/
