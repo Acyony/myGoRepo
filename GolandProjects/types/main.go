867184118cb971c9e9fb86c3771d099fbde5d66a
Alcione <alcionefribeiro@gmail.com>
2021-12-22 19:02:27 +0100

    learning slice

--- GolandProjects/types/main.go
+++ GolandProjects/types/main.go
@@ -1,27 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // reference types(pointers, slices, maps, functions, channels)
-// pointers => something that points to a specific location  in memory
 // interface type
 
 func main() {
-	x := 10
-	// hold the address where x is storage in the memory
-	// &x reference to a pointer
-	// *myFirstPointer to get the value and change it
+	var animals []string
+	animals = append(animals, "dog")
+	animals = append(animals, "fish")
+	animals = append(animals, "cat")
+	animals = append(animals, "horse")
+	fmt.Println(animals)
 
-	myFirstPointer := &x
-	fmt.Println("x is", x)
-	fmt.Println("myFirstPointer is", myFirstPointer)
+	// _, x index and the current item in the slice i'm looking at
 
-	*myFirstPointer = 15
-	fmt.Println("x is now", x)
-	changeValueOfPointer(&x)
-	fmt.Println("After function call, x is now", x)
+	for i, x := range animals {
+		fmt.Println(i, x)
+	}
+
+	fmt.Println("Element 0 is", animals[0])
+	fmt.Println("First two elements are", animals[0:2])
+	fmt.Println("The slice is", len(animals), "elements long")
+
+	// sorting slices strings
+	fmt.Println("Is it sorted?", sort.StringsAreSorted(animals))
+	sort.Strings(animals)
+	fmt.Println("Is it sorted now?", sort.StringsAreSorted(animals))
+	fmt.Println(animals)
+	animals = DeleteFromSlice(animals, 1)
+	fmt.Println(animals)
 }
 
-func changeValueOfPointer(num *int) {
-	*num = 25
+func DeleteFromSlice(a []string, i int) []string {
+	//deleting an element using the index
+	a[i] = a[len(a)-1] // copy the last element of the slice
+	a[len(a)-1] = ""   // given the last element a default value
+	a = a[:len(a)-1]   // deleting the last element
+	return a
 }
