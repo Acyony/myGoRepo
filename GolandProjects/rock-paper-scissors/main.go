5b291bed8aceb1b10eeff3582ce7022f8030c712
Alcione <alcionefribeiro@gmail.com>
2022-01-03 22:28:19 +0100

    add some changes to the game

--- GolandProjects/rock-paper-scissors/main.go
+++ GolandProjects/rock-paper-scissors/main.go
@@ -21,32 +21,120 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	playerChoice := ""
 	playerValue := -1
-
-	computerValue := rand.Intn(3)
+	// *** added two variables to keep track of score
+	playerScore := 0
+	computerScore := 0
 
 	reader := bufio.NewReader(os.Stdin)
 
 	clearScreen()
 
-	fmt.Print("Please enter rock, paper, or scissors -> ")
-	playerChoice, _ = reader.ReadString('\n')
-	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
-
-	if playerChoice == "rock" {
-		playerValue = ROCK
-	}
-
-	if playerChoice == "paper" {
-		playerValue = PAPER
-	}
-
-	if playerChoice == "scissors" {
-		playerValue = SCISSORS
-	}
-
+	// *** print out some instructions
+	fmt.Print("Rock, Paper & Scissors")
+	fmt.Println("----------------------")
+	fmt.Println("Game is played for three rounds, and best of three wins the game. Good Luck!")
 	fmt.Println()
-	fmt.Println("Player chose", playerChoice, "and value is", playerValue)
-	fmt.Println("Computer value is", computerValue)
+
+	// *** added the for loop
+	for i := 1; i <= 3; i++ {
+		// *** print out the information
+		fmt.Println()
+		fmt.Println("Round", i)
+		fmt.Println("---------------")
+
+		fmt.Print("Please enter rock, paper, or scissors -> ")
+		playerChoice, _ = reader.ReadString('\n')
+		playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+
+		// moved computer choice to for loop, so it's reset each time through
+		computerValue := rand.Intn(3)
+
+		if playerChoice == "rock" {
+			playerValue = ROCK
+		} else if playerChoice == "paper" {
+			playerValue = PAPER
+		} else if playerChoice == "scissors" {
+			playerValue = SCISSORS
+		} else {
+			// *** reset playerValue to -1 if invalid choice
+			playerValue = -1
+		}
+
+		// *** print out player choice
+		fmt.Println()
+		fmt.Println("Player chose", strings.ToUpper(playerChoice))
+
+		switch computerValue {
+		case ROCK:
+			fmt.Println("Computer chose ROCK!")
+			break
+		case PAPER:
+			fmt.Println("Computer chose PAPER!")
+			break
+		case SCISSORS:
+			fmt.Println("Computer chose SCISSORS!")
+			break
+		default:
+		}
+
+		//fmt.Println("Player value is", playerValue)
+		fmt.Println()
+		if playerValue == computerValue {
+			fmt.Println("It's a draw!")
+			// *** decrement i by 1, since we're repeating the round
+			i--
+		} else {
+			// *** refactor the logic to keep track of score and print messages to two new functions, computerWins and playerWins
+
+			switch playerValue {
+			case ROCK:
+				if computerValue == PAPER {
+					computerScore = computerWins(computerScore)
+				} else {
+					playerScore = playerWins(playerScore)
+				}
+				break
+			case PAPER:
+				if computerValue == SCISSORS {
+					computerScore = computerWins(computerScore)
+				} else {
+					playerScore = playerWins(playerScore)
+				}
+				break
+			case SCISSORS:
+				if computerValue == ROCK {
+					computerScore = computerWins(computerScore)
+				} else {
+					playerScore = playerWins(playerScore)
+				}
+				break
+			default:
+				// *** decrement i by 1, since we're repeating the round
+				fmt.Println("Invalid choice!")
+				i--
+			}
+		}
+	}
+
+	fmt.Println("Final score")
+	fmt.Println("--------------------")
+	fmt.Printf("Player: %d/3, Computer %d/3", playerScore, computerScore)
+	fmt.Println()
+	if playerScore > computerScore {
+		fmt.Println("Player wins the game!")
+	} else {
+		fmt.Println("Computer wins the game!")
+	}
+}
+
+func computerWins(score int) int {
+	fmt.Println("Computer wins!")
+	return score + 1
+}
+
+func playerWins(score int) int {
+	fmt.Println("Player wins!")
+	return score + 1
 }
 
 // clearScreen clears the screen
