0b669badabfa4d1a838f51e7e5688c10f4d8f072
Alcione <alcionefribeiro@gmail.com>
2022-01-05 20:35:47 +0100

    primary Operators

--- GolandProjects/operators/main.go
+++ GolandProjects/operators/main.go
@@ -1,12 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	answer := 7 + 3*8
+	/*answer := 7 + 3*8
 	fmt.Println("Answer =", answer)
-
 	answer = (7 + 3) * 8
-	fmt.Println("Answer is now =", answer)
+	fmt.Println("Answer is now =", answer)*/
 
+	// multiplication
+	var radius = 12.0
+	area := math.Pi * radius * radius
+	fmt.Println("Area is =", area)
+
+	// integer division
+	half := 1 / 2
+	fmt.Println("Half integer division =", half)
+
+	halfFloat := 1.0 / 2.0
+	fmt.Println("Half float is =", halfFloat)
+
+	// squaring (raising to the power)
+	badThreeSquared := 3 ^ 2
+	fmt.Println("Bad Three Squared =", badThreeSquared)
+	fmt.Println("-------=^.^=-------")
+	goodThreeSquared := math.Pow(3.0, 2.0)
+	fmt.Println("Good Three Squared =", goodThreeSquared)
+
+	// modules
+	remainder := 50 % 3
+	fmt.Println("remainder =", remainder)
+
+	// unary operators
+
+	x := 3
+	x++
+	fmt.Println("x is now =", x)
+
+	x--
+	x--
+	fmt.Println("x is now =", x)
 }
