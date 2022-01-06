9bbe14e18537b8d45eb9f78e2040e6e91a327ce0
Alcione <alcionefribeiro@gmail.com>
2022-01-06 09:10:09 +0100

    learning precedence

--- GolandProjects/operators/main.go
+++ GolandProjects/operators/main.go
@@ -1,46 +1,81 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
 	/*answer := 7 + 3*8
 	fmt.Println("Answer =", answer)
 	answer = (7 + 3) * 8
 	fmt.Println("Answer is now =", answer)*/
+	/*
+		// multiplication
+		var radius = 12.0
+		area := math.Pi * radius * radius
+		fmt.Println("Area is =", area)
 
-	// multiplication
-	var radius = 12.0
-	area := math.Pi * radius * radius
-	fmt.Println("Area is =", area)
+		// integer division
+		half := 1 / 2
+		fmt.Println("Half integer division =", half)
+
+		halfFloat := 1.0 / 2.0
+		fmt.Println("Half float is =", halfFloat)
+
+		// squaring (raising to the power)
+		badThreeSquared := 3 ^ 2
+		fmt.Println("Bad Three Squared =", badThreeSquared)
+		fmt.Println("-------=^.^=-------")
+		goodThreeSquared := math.Pow(3.0, 2.0)
+		fmt.Println("Good Three Squared =", goodThreeSquared)
+
+		// modules
+		remainder := 50 % 3
+		fmt.Println("remainder =", remainder)
+
+		// unary operators
+
+		x := 3
+		x++
+		fmt.Println("x is now =", x)
+
+		x--
+		x--
+		fmt.Println("x is now =", x)*/
+
+	// reference source
+	// https://www.tutorialspoint.com/go/go_operators_precedence.htm
+	// https://www.golangprograms.com/arithmetic-operators-in-go-programming-language.html
+	// https://go.dev/ref/spec#Arithmetic_operators
+
+	// precedents
+
+	a := 12.0 * 3.0 / 4.0
+	b := (12.0 * 3.0) / 4.0
+	c := 12.0 * (3.0 / 4.0)
+	fmt.Println("a =", a, "b =", b, "c =", c)
 
 	// integer division
-	half := 1 / 2
-	fmt.Println("Half integer division =", half)
+	unclear := 12 * (3 / 4)
+	fmt.Println("unclear =", unclear)
 
-	halfFloat := 1.0 / 2.0
-	fmt.Println("Half float is =", halfFloat)
-
-	// squaring (raising to the power)
-	badThreeSquared := 3 ^ 2
-	fmt.Println("Bad Three Squared =", badThreeSquared)
+	// parenthesis
+	foo := 12.0 / 3.0 / 40
+	fmt.Println("foo =", foo)
 	fmt.Println("-------=^.^=-------")
-	goodThreeSquared := math.Pow(3.0, 2.0)
-	fmt.Println("Good Three Squared =", goodThreeSquared)
 
-	// modules
-	remainder := 50 % 3
-	fmt.Println("remainder =", remainder)
+	foo1 := 12.0 / (3.0 / 40)
+	fmt.Println("foo1 =", foo1)
 
-	// unary operators
+	// addition/subtraction
+	fmt.Println()
 
-	x := 3
-	x++
-	fmt.Println("x is now =", x)
+	x := 12 + 3 - 4
+	y := (12 + 3) - 4
+	z := 12 + (3 - 4)
+	fmt.Println("x =", x, "y =", y, "z =", z)
+	fmt.Println("-------=^.^=-------")
 
-	x--
-	x--
-	fmt.Println("x is now =", x)
+	x = 12 + 3*4
+	y = (12 + 3) * 4
+	z = 12 + (3 * 4)
+	fmt.Println("x =", x, "y =", y, "z =", z)
 }
