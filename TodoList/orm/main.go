1b8710176693000aedc7023bc00a85a75b70cfc4
Alcione <alcionefribeiro@gmail.com>
2022-03-22 21:39:39 +0100

    Implemeting funtions to List, Create, add todos

--- TodoList/orm/main.go
+++ TodoList/orm/main.go
@@ -15,10 +15,25 @@ type Todo struct {
 func ListTodos(db *gorm.DB) ([]*Todo, error) {
 	todos := []*Todo{}
 	result := db.Find(&todos)
-
 	return todos, result.Error
 }
 
+func CompleteTodo(db *gorm.DB, id int) error {
+	result := db.Model(&Todo{}).Where("id=?", id).Update("is_completed", true)
+	return result.Error
+}
+
+func AddNewTodo(db *gorm.DB, title string) error {
+	todo := &Todo{
+		Title:       title,
+		IsCompleted: false,
+		CreatedAt:   time.Now(),
+	}
+
+	result := db.Create(todo)
+
+	return result.Error
+}
 func main() {
 
 }
